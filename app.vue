839b80427cd06b79ac619f34992989beeb68b41e
steffkes <stefan@gelungen.es>
2024-01-11 10:37:29 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="background-color: #f9f9f9">
+    <NuxtPage />
+  </div>
+</template>
+<script setup>
+const event = {
+  "@context": "https://schema.org",
+  "@type": "SportsEvent",
+  name: "Treppe hoch 4",
+  description: "Spendenlauf: Treppenlauf im Ausdauerformat",
+  startDate: "2024-03-02",
+  endDate: "2024-03-02",
+  eventStatus: "https://schema.org/EventScheduled",
+  eventAttendanceMode: "https://schema.org/OfflineEventAttendanceMode",
+  location: {
+    "@type": "Place",
+    name: "Maritim Hotel Ulm",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "Basteistraße 40",
+      postalCode: "89073",
+      addressLocality: "Ulm",
+      addressRegion: "BW",
+      addressCountry: "DE",
+    },
+  },
+  image: ["/maritim-ulm.jpg"],
+};
+
+const formattedDate = new Date(event.startDate).toLocaleDateString(
+  "de-DE",
+  {
+    weekday: "long",
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+  }
+);
+
+
+useHead({
+  titleTemplate: (pageTitle) =>
+    [pageTitle, event.name].filter(Boolean).join(" | "),
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content: formattedDate + " @ " + event.location.name,
+    },
+    {
+      property: "og:image",
+      content: event.image[0],
+    },
+    { property: "og:type", content: "website" },
+    { name: "twitter:card", content: "summary_large_image" },
+  ],
+  script: [
+    {
+      type: "application/ld+json",
+      children: JSON.stringify(event),
+    },
+  ],
+});
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,295 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="box is-shadowless">
+        <div class="content">
+          <h2 class="title is-3">Die Fakten</h2>
+
+          <table id="facts" class="table" style="width: auto">
+            <tbody>
+              <tr>
+                <th>Was?</th>
+                <td>Spendenlauf (Treppenlauf im Ausdauerformat)</td>
+              </tr>
+              <tr>
+                <th>Für?</th>
+                <td>
+                  Förderkreis Ulm für tumor- und leukämiekranke Kinder Ulm e.V.
+                </td>
+              </tr>
+              <tr>
+                <th>Wann?</th>
+                <td>Samstag, 02.03.24 09:30 - 19:00 Uhr</td>
+              </tr>
+              <tr>
+                <th>Wo?</th>
+                <td>Maritim Hotel Ulm</td>
+              </tr>
+              <tr>
+                <th>Wer?</th>
+                <td>Kostenlose Teilnahme auf Einladung</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h2 class="title is-3">Die Idee</h2>
+
+          <p>
+            Der erste dokumentierte Treppenlauf-Wettkampf mit Zeitnahme fand im
+            Jahre 1905 statt (behauptet zumindest Wikipedia). So richtig los
+            ging es dann 1978 in New York City auf dem Empire State Building.
+            Seit dem gibt es weltweit Treppenläufe in verschiedenster Form: Egal
+            ob der CN Tower in Toronto, der Taipei 101 in Taipeh, im Park-Inn
+            Hotel in Berlin oder draußen auf der Heini-Klopfer-Schanze in
+            Oberstdorf.
+          </p>
+
+          <p>
+            Dabei führt die Laufenden der Weg allerdings nur einmalig nach oben
+            - und dann war's das schon mit dem Vergnügen. Durch einen Zufall
+            hatte unser Initiator Stefan die Möglichkeit beim
+            <a href="http://firestairrun-pb.de/"
+              >Firefighter Charity Stairrun Paderborn</a
+            >
+            mitzulaufen .. und zwar den ganzen Tag (naja zumindest fast: Acht
+            Stunden waren es, für alle die es ganz genau wissen wollen).
+          </p>
+
+          <p>
+            Warum in die Ferne schweifen, wenn das Gute liegt so nah: Auch in
+            Ulm gibt es einige hübsche Treppen die sich für eine solche Quälerei
+            anbieten. Und ehe wir uns versahen, waren wir mit unserer Idee nicht
+            mehr alleine. Um dem Ganzen etwas Struktur zu geben, laufen bei uns
+            jeweils vier Personen in einer Gruppe: Ein Kind, ein Erwachsener,
+            ein Towerrunner (ja, die machen das öfter) und eine Rettungskraft
+            (Feuerwehr, Polizei, THW, ..). Für jede absolvierte Runde (davon
+            gibt es insgesamt 32) hat ein Sponsor zugesagt einen bestimmten
+            Geldbetrag zu spenden.
+          </p>
+
+          <h2 class="title is-3">Der Empfänger</h2>
+          <p>
+            <a href="https://www.foerderkreis-ulm.org"
+              >Förderkreis Ulm für tumor- und leukämiekranke Kinder Ulm e.V.
+            </a>
+            Hier folgen weitere Informationen zur Tätigkeit des FKU
+          </p>
+
+          <h2 class="title is-3">Die Läufer</h2>
+          <p>Content für die Läufer</p>
+
+          <div class="columns is-vcentered grid">
+<div class="column">1</div>
+<div class="column">2</div>
+<div class="column">3</div>
+<div class="column">4</div>
+<div class="column">5</div>
+</div>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>Gruppe</th>
+      <th>K<span>ind</span></th>
+      <th>E<span>rwachsener</span></th>
+      <th>T<span>owerrunner</span></th>
+      <th>R<span>ettungskraft</span></th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th>Gruppe</th>
+      <th>K<span>ind</span></th>
+      <th>E<span>rwachsener</span></th>
+      <th>T<span>owerrunner</span></th>
+      <th>R<span>ettungskraft</span></th>
+    </tr>
+  </tfoot>
+  <tbody>
+    <tr>
+      <th>1</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>2</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>3</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>4</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+        <tr>
+      <th>5</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>6</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>7</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+    <tr>
+      <th>8</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+       <tr>
+      <th>9</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+        <tr>
+      <th>10</th>
+      <td>K</td>
+      <td>E</td>
+      <td>T</td>
+      <td>R</td>
+    </tr>
+  </tbody>
+</table>
+
+
+          <h2 class="title is-3">Die Sponsoren</h2>
+
+          <div class="columns is-multiline is-vcentered grid">
+            <div class="column is-4 has-text-centered">
+              <img src="/sponsoren/beurer.svg" />
+            </div>
+            <div class="column is-4 has-text-centered">
+              <img src="/sponsoren/esta.svg" class="reduced" />
+            </div>
+            <div class="column is-4 has-text-centered">
+              <img src="/sponsoren/evobus.svg" />
+            </div>
+          </div>
+          <div class="columns is-multiline is-vcentered grid">
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/gardena.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/kaessbohrer.svg" class="reduced" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/magirus.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/noerpel.svg" class="reduced" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/schwenk.svg" class="reduced" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/sparkasse-ulm.svg" class="invert reduced" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/teva.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/ulrich-medical.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/uzin-utz.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/wieland.svg" />
+            </div>
+            <div class="column is-2 has-text-centered">
+              <img src="/sponsoren/zwick-roell.svg" class="reduced" />
+            </div>
+          </div>
+
+          <div class="is-clearfix">
+            <h2 class="title is-3">Die Location</h2>
+
+            <div class="columns">
+              <div class="column is-2">
+                <img src="/maritim-ulm.jpg" alt="" />
+              </div>
+              <div>
+                Das
+                <a href="https://www.maritim.de/de/hotels/deutschland/hotel-ulm"
+                  >Maritim Hotel Ulm</a
+                >
+                liegt direkt an der Donau und eröffnet, insbesondere vom
+                Restaurant in der 16. Etage, einen fantastischen Blick auf das
+                Ulmer Münster sowie ganz Ulm und Neu-Ulm. Doch auch das Hotel
+                selbst kann mit tollen Einblicken glänzen.
+              </div>
+            </div>
+          </div>
+          <h2 class="title is-3">Die Partner</h2>
+
+          <div class="columns is-multiline is-vcentered grid">
+            <div class="column is-4 has-text-centered">
+              <img src="/partner/maritim.svg" />
+            </div>
+            <div class="column is-4 has-text-centered">
+              <img src="/partner/donau3fm.svg" />
+            </div>
+            <div class="column is-4 has-text-centered">
+              <img src="/partner/mueller.svg" />
+            </div>
+          </div>
+
+          <h2 class="title is-3">Die Möglichkeiten</h2>
+          <p>Content für die Möglichkeiten</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.content h2:not(:first-child) {
+  margin-top: 2em;
+}
+.title:not(:last-child) {
+  margin-bottom: 0.5rem;
+}
+
+#facts th {
+  text-align: right;
+}
+
+.grid img {
+  /*aspect-ratio: 3/2;*/
+  filter: grayscale(1);
+}
+.grid img.invert {
+  filter: invert(1);
+}
+.grid img.reduced {
+  opacity: 0.7;
+}
+</style>
