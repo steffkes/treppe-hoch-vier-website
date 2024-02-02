e2fb23c7599c1a2ba7360d8091e0026f10f40be0
steffkes <stefan@gelungen.es>
2024-02-02 20:17:10 +0100

    initial information for participants

--- pages/teilnehmer.vue
+++ pages/teilnehmer.vue
@@ -28,6 +28,67 @@
 
       <div class="box is-shadowless">
         <div class="content">
+          <h2 class="title is-3">Die Fakten</h2>
+
+          <table id="facts" class="table" style="width: auto">
+            <tbody>
+              <tr>
+                <th>Was?</th>
+                <td>Spendenlauf (Treppenlauf im Ausdauerformat)</td>
+              </tr>
+              <tr>
+                <th>Für?</th>
+                <td>
+                  Förderkreis für tumor- und leukämiekranke Kinder Ulm e.V.
+                </td>
+              </tr>
+              <tr>
+                <th>Wann?</th>
+                <td>Samstag, 02.03.24 09:30 - 19:00 Uhr</td>
+              </tr>
+              <tr>
+                <th>Wo?</th>
+                <td>Maritim Hotel Ulm</td>
+              </tr>
+              <tr>
+                <th>Wer?</th>
+                <td>Kostenlose Teilnahme auf Einladung</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h2 class="title is-3">Die Herausforderung</h2>
+
+          <p>
+            Ein Kind, ein Erwachsener, ein Towerrunner (professioneller
+            Turmläufer) und eine Rettungskraft (Feuerwehr, Polizei, THW, ...)
+            bilden eine Gruppe. Diese vier Personen laufen acht Stunden lang,
+            vier mal pro Stunde Treppen hoch und runter:
+            <strong>TREPPE HOCH 4</strong>!
+          </p>
+
+          <p>
+            <img
+              src="/zeitachse.svg"
+              alt="Grafik die den zeitlichen Ablauf von TREPPE HOCH 4 erklärt: Kinder laufen 20 Minuten vor der vollen Stunde, Erwachsene laufen jeweils zur vollen Stunde los."
+            />
+          </p>
+
+          <p>
+            Was zunächst einfach klingt, ist eine echte Herausforderung. Durch
+            die Dauerbelastung mit nur kurzen Pausen, machen irgendwann die
+            Beine schlapp und dem Körper reicht es auch. Einzig der Kopf sagt:
+            Durchhalten!
+          </p>
+
+          <p>
+            Die Motivation kommt durch die Gruppe. Keiner ist alleine! Alle
+            laufen für das gleiche Ziel: möglichst viele Runden zu schaffen um
+            möglichst viele Spenden für einen guten Zweck zu sammeln. Denn das
+            ist es, was wir mit unserem Lauf erreichen wollen und die Idee, die
+            hinter diesem Treppenmarathon steckt.
+          </p>
+
           <p>
             <em>Weitere Informationen für Teilnehmer folgen in Kürze ...</em>
           </p>
